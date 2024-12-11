af697e5cd302ce60a662846030631993753c0e3f
Keshia-w <keshia.w196@gmail.com>
2024-12-11 13:15:43 +1300

    Basic UI layout

---
+++ countdown/src/Components/LetterCardComponent.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+    Box,
+    Text,
+    Flex,
+    Card,
+    CardBody
+
+} from '@chakra-ui/react'
+ 
+function Letters ()  {
+    return(
+        <Box>
+            <Flex justifyContent={"space-between"}>
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+
+                <Card w="100px" h="100px">
+                    <Text fontSize="60px"></Text>
+                </Card>
+            </Flex>
+        </Box>
+    )
+}
+
+export default Letters;
